319358e7778e3fd3d17570988e4c158015f06f06
Sandeep Yadav <rkysandeepyadav@gmail.com>
2025-04-24 14:09:41 +0530

    Nav, Footer

---
+++ real-time-track/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+      <h1>Real Time Track</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
